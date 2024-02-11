733df5d56195952eb6fd42dc27272ab2fc02eb71
Hama <mohamedbenjedida18@gmail.com>
2024-02-11 18:29:37 +0100

    added dropdown component

---
+++ src/modules/shared/components/DropDown/DropDown.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useRef, useEffect, ReactElement } from 'react'
+export interface DropdownItem {
+  key?: string
+  label?: string | ReactElement
+  onClick: () => void
+  disabled?: boolean
+}
+
+export interface DropdownProps {
+  items: DropdownItem[]
+  placement?: 'bottomRight' | 'bottomLeft'
+  triggerElement?: ReactElement
+  isOpen: boolean
+  setIsOpen: any
+}
+
+const Dropdown: React.FC<DropdownProps> = ({
+  items,
+  placement = 'bottomRight',
+  triggerElement,
+  isOpen,
+  setIsOpen,
+}) => {
+  //   const [isOpen, setIsOpen] = useState(false)
+  const dropdownRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
+  return (
+    <div className="dropdown" ref={dropdownRef}>
+      {triggerElement}
+      {isOpen && (
+        <div className={`dropdown-menu ${placement}`}>
+          {items.map((item) => (
+            <div
+              key={item.key}
+              className="dropdown-item"
+              onClick={() => {
+                item.onClick()
+                setIsOpen(false)
+              }}
+            >
+              {item.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Dropdown
--- src/modules/shared/components/Navbar/Navbar.tsx
+++ src/modules/shared/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import menuIcon from '../../assets/icons/navbar/menu.svg'
 import { useLocation } from 'react-router-dom'
 import ThemeButton from '../ThemeButton/ThemeButton'
-import { Avatar, Button, Dropdown, MenuProps, Space } from 'antd'
+import { Avatar, Button, Space } from 'antd'
 import enFlagIcon from '../../assets/icons/Navbar/en-flag.png'
 import frFlagIcon from '../../assets/icons/Navbar/fr-flag.png'
 import arFlagIcon from '../../assets/icons/Navbar/ar-flag.png'
@@ -12,6 +12,7 @@ import { useAppDispatch } from '../../store'
 import { logout } from '@src/modules/auth/data/authThunk'
 import { useTranslation } from 'react-i18next'
 import { useState } from 'react'
+import Dropdown from '../DropDown/DropDown'
 
 interface INavbarProps {
   setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>
@@ -29,6 +30,8 @@ const Navbar: React.FC<INavbarProps> = ({
   const { t, i18n } = useTranslation('translation')
 
   const [lang, setLang] = useState(i18n?.language?.toString())
+  const [isOpen, setIsOpen] = useState(false)
+  const [isSettingOpen, setIsSettingOpen] = useState(false)
 
   const onChangeLanguage = (language: string) => {
     i18n.changeLanguage(language)
@@ -39,37 +42,40 @@ const Navbar: React.FC<INavbarProps> = ({
     dispatch(logout())
   }
 
-  const languagesItems: MenuProps['items'] = [
+  const languagesItems = [
     {
       key: '1',
       label: (
-        <div className="navbar-flag-container" onClick={() => onChangeLanguage('en')}>
+        <div className="navbar-flag-container">
           <img src={enFlagIcon} alt="flag" className="navbar-flag" />
           <p>{t('language.en')}</p>
         </div>
       ),
+      onClick: () => onChangeLanguage('en'),
     },
     {
       key: '2',
       label: (
-        <div className="navbar-flag-container" onClick={() => onChangeLanguage('fr')}>
+        <div className="navbar-flag-container">
           <img src={frFlagIcon} alt="flag" className="navbar-flag" />
           <p>{t('language.fr')}</p>
         </div>
       ),
+      onClick: () => onChangeLanguage('fr'),
     },
     {
       key: '3',
       label: (
-        <div className="navbar-flag-container" onClick={() => onChangeLanguage('ar')}>
+        <div className="navbar-flag-container">
           <img src={arFlagIcon} alt="flag" className="navbar-flag" />
           <p>{t('language.ar')}</p>
         </div>
       ),
+      onClick: () => onChangeLanguage('ar'),
     },
   ]
 
-  const accountInfoItems: MenuProps['items'] = [
+  const accountInfoItems = [
     {
       key: '1',
       label: (
@@ -84,24 +90,27 @@ const Navbar: React.FC<INavbarProps> = ({
         </Space>
       ),
       disabled: true,
+      onClick: () => console.log('user info'),
     },
     {
       key: '2',
       label: <p>Profile</p>,
       icon: <ProfileIcon style={{ stroke: 'black', width: '18px', height: '18px' }} />,
+      onClick: () => console.log('profile'),
     },
     {
       key: '3',
       label: <p>Settings</p>,
       icon: <SettingsIcon style={{ stroke: 'black', width: '18px', height: '18px' }} />,
+      onClick: () => console.log('Settings'),
     },
     {
       key: '4',
       label: <p onClick={handleLogout}>logout</p>,
       icon: <LogoutIcon style={{ stroke: 'black', width: '18px', height: '18px' }} />,
+      onClick: () => console.log('logout'),
     },
   ]
-
   return (
     <div className="navbar">
       <div className="navbar-left">
@@ -123,40 +132,35 @@ const Navbar: React.FC<INavbarProps> = ({
         <p className="navbar-left-title">{pathname.split('/')[1]}</p>
       </div>
       <div className="navbar-right">
-        <Space size={'middle'}>
-          <Dropdown
-            menu={{ items: languagesItems }}
-            trigger={['click']}
-            placement="bottomRight"
-            arrow
-          >
-            <Button type="link" shape="circle">
-              <div className="navbar-flag-container">
-                <img
-                  src={lang === 'en' ? enFlagIcon : lang === 'fr' ? frFlagIcon : arFlagIcon}
-                  alt="flag"
-                  className="navbar-flag"
-                />
-              </div>
-            </Button>
-          </Dropdown>
-
-          <Dropdown
-            menu={{ items: accountInfoItems }}
-            trigger={['click']}
-            placement="bottomRight"
-            arrow
-            className="navbar-dropdown-cursor"
-          >
-            <Space>
+        <Dropdown
+          isOpen={isSettingOpen}
+          setIsOpen={setIsSettingOpen}
+          items={accountInfoItems}
+          placement="bottomRight"
+          triggerElement={
+            <button onClick={() => setIsSettingOpen(true)} className="navbar-avatar-btn">
               <Avatar size={32} className="navbar-avatar">
                 TG
               </Avatar>
-            </Space>
-          </Dropdown>
-        </Space>
-
+            </button>
+          }
+        ></Dropdown>
         <ThemeButton />
+        <Dropdown
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          items={languagesItems}
+          placement="bottomRight"
+          triggerElement={
+            <button className="navbar-flag-container" onClick={() => setIsOpen(true)}>
+              <img
+                src={lang === 'en' ? enFlagIcon : lang === 'fr' ? frFlagIcon : arFlagIcon}
+                alt="flag"
+                className="navbar-flag"
+              />
+            </button>
+          }
+        ></Dropdown>
       </div>
     </div>
   )
