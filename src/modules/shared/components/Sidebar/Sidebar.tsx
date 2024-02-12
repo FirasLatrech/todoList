2c6c8d21d1d04c33f2353b1cf1be723dc9ad0df5
Hama <mohamedbenjedida18@gmail.com>
2024-02-12 10:30:20 +0100

    changed layout

--- src/modules/shared/components/Sidebar/Sidebar.tsx
+++ src/modules/shared/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
 import SidebarItems from '../SidebarItems/SidebarItems'
+import  LogoIcon  from '../../assets/icons/sidebar/logo.png'
+import  smallLogoIcon  from '../../assets/icons/sidebar/smallLogo.png'
 
 interface ISidebarProps {
   collapseSidebar: boolean
@@ -7,9 +9,9 @@ interface ISidebarProps {
 const Sidebar: React.FC<ISidebarProps> = ({ collapseSidebar }) => {
   return (
     <div className={`sidebar ${collapseSidebar ? 'collapse' : ''}`}>
-      <div className="sidebar-logo-container">{collapseSidebar ? 'L' : 'Logo'}</div>
 
       <div className="sidebar-content">
+      <div className="sidebar-logo-container">{collapseSidebar ?  <img src={smallLogoIcon} /> : <img src={LogoIcon} />}</div> 
         <div className="sidebar-nav-items">
           <SidebarItems collapseSidebar={collapseSidebar} />
         </div>
