98f32bc0030f8bfc5061febde9711389b3d56916
Hama <mohamedbenjedida18@gmail.com>
2024-02-12 15:23:22 +0100

    fixed side ba

--- src/modules/shared/components/Sidebar/Sidebar.tsx
+++ src/modules/shared/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,19 @@
 import SidebarItems from '../SidebarItems/SidebarItems'
 import  LogoIcon  from '../../assets/icons/sidebar/logo.png'
 import  smallLogoIcon  from '../../assets/icons/sidebar/smallLogo.png'
+import { useTranslation } from 'react-i18next'
 
 interface ISidebarProps {
   collapseSidebar: boolean
 }
 
 const Sidebar: React.FC<ISidebarProps> = ({ collapseSidebar }) => {
-  return (
-    <div className={`sidebar ${collapseSidebar ? 'collapse' : ''}`}>
+  const {  i18n } = useTranslation('translation')
 
-      <div className="sidebar-content">
+  return (
+    <div className={`sidebar ${collapseSidebar ? 'collapse' : ''}`} >
+
+      <div className="sidebar-content" lang={i18n?.language}>
       <div className="sidebar-logo-container">{collapseSidebar ?  <img src={smallLogoIcon} /> : <img src={LogoIcon} />}</div> 
         <div className="sidebar-nav-items">
           <SidebarItems collapseSidebar={collapseSidebar} />
